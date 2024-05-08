c34a3dac7d12b6123ee958ae5a34f587d0f1ad6e
nathan-desilva <159185114+nathan-desilva@users.noreply.github.com>
2024-05-08 18:06:26 +0000

    added js

--- js/script.js
+++ js/script.js
@@ -1,5 +1,19 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
+// Copyright (c) 2024 Nathan De Silva All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Nathan De Silva
+// Created on: May 2024
 // This file contains the JS functions for index.html
+
+"use strict"
+
+function myButtonClicked() {
+  // input
+  const number = parseInt(document.getElementById('number').value)
+  let answer = 0
+
+  // process
+  for (let counter = 0; counter <= number; counter++) {
+    answer += counter
+  }
+  document.getElementById('answer').innerHTML = "The sum of all the numbers from 0 to " + number + " is: " + answer
+}
