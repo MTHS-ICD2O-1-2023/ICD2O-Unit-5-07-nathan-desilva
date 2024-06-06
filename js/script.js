88c3af5bf3be92f32f039d2226b8a8bf40db3a40
nathan-desilva <159185114+nathan-desilva@users.noreply.github.com>
2024-06-06 18:01:08 +0000

    edited js

--- js/script.js
+++ js/script.js
@@ -4,17 +4,21 @@
 // Created on: May 2024
 // This file contains the JS functions for index.html
 
-"use strict"
-
 function myButtonClicked() {
   // input
   const params = new URLSearchParams(document.location.search)
-  const number = params.get('number')
+  const number = params.get('number') || 0
   let answer = 0
 
   // process
   for (let counter = 0; counter <= number; counter++) {
     answer += counter
   }
+
+  // add query string to URL
+  const newURL = new URL(document.location)
+  newURL.searchParams.set('answer', answer)
+  document.location.href = newURL.href
+
   document.getElementById('answer').innerHTML = "The sum of all the numbers from 0 to " + number + " is: " + answer
 }
